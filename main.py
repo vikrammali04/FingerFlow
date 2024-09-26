e5433ecaa764c4d78b891708ab67825ffe35cbf5
Vikram <vsmali4119@gmail.com>
2024-09-27 00:00:42 +0530

    complete poc

---
+++ main.py
@@ -0,0 +1,77 @@
+import cv2
+import mediapipe
+from utils import ColorDrawer
+
+def main():
+    hand_detector = mediapipe.solutions.hands.Hands(model_complexity=1,
+                                                   min_detection_confidence=0.4,
+                                                   min_tracking_confidence=0.4)
+    cap = cv2.VideoCapture(0)
+    drawer = ColorDrawer()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = hand_detector.process(frame_rgb)
+        hands = result.multi_hand_landmarks
+
+        # Drawing and color selection logic
+        if hands:
+            landmarks = []
+            for hand in hands:
+                detected_landmarks = hand.landmark
+                for landmark in detected_landmarks:
+                    lmx = int(landmark.x * 640)
+                    lmy = int(landmark.y * 480)
+                    landmarks.append([lmx, lmy])
+
+            fore_finger = (landmarks[8][0], landmarks[8][1])
+            thumb = (landmarks[4][0], landmarks[4][1])
+
+            # Draw the fingertip
+            cv2.circle(frame, fore_finger, 3, drawer.colors[drawer.colorIndex], -1)
+
+            # Button interaction logic
+            if (thumb[1] - fore_finger[1] < 30):
+
+                drawer.add_next_deques()
+            elif fore_finger[1] <= 65:
+                # In button mode
+                if 40 <= fore_finger[0] <= 140:  # Clear Button
+                    drawer.clear_canvas()
+                elif 160 <= fore_finger[0] <= 260:
+                    drawer.update_color_index(0)  # Blue
+                elif 280 <= fore_finger[0] <= 380:
+                    drawer.update_color_index(1)  # Green
+                elif 400 <= fore_finger[0] <= 500:
+                    drawer.update_color_index(2)  # Red
+            else:
+                drawer.add_point(fore_finger)
+        else:
+            drawer.add_next_deques()
+
+        # Draw lines of all the colors on the canvas and frame
+        points = drawer.get_points()
+
+        for i in range(len(points)):
+            for j in range(len(points[i])):
+                for k in range(1, len(points[i][j])):
+                    if points[i][j][k - 1] is None or points[i][j][k] is None:
+                        continue
+                    cv2.line(frame, points[i][j][k - 1], points[i][j][k], drawer.colors[i], 4)
+                    cv2.line(drawer.paintWindow, points[i][j][k - 1], points[i][j][k], drawer.colors[i], 4)
+
+        cv2.imshow("Canvas", drawer.paintWindow)
+        cv2.imshow("Camera", drawer.update_camera_ui(frame=frame))
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+from collections import deque
+
+class ColorDrawer:
+    def __init__(self):
+        self.blue = [deque(maxlen=1024)]
+        self.green = [deque(maxlen=1024)]
+        self.red = [deque(maxlen=1024)]
+        self.blue_index = 0
+        self.green_index = 0
+        self.red_index = 0
+        self.colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        self.colorIndex = 0
+        self.paintWindow = np.ones((471, 636, 3))
+
+        self.setup_paint_window()
+
+    def setup_paint_window(self):
+        self.paintWindow = cv2.rectangle(self.paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
+        self.paintWindow = cv2.rectangle(self.paintWindow, (160, 1), (260, 65), (255, 0, 0), 2)
+        self.paintWindow = cv2.rectangle(self.paintWindow, (280, 1), (380, 65), (0, 255, 0), 2)
+        self.paintWindow = cv2.rectangle(self.paintWindow, (400, 1), (500, 65), (0, 0, 255), 2)
+        cv2.putText(self.paintWindow, "CLEAR", (60, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.paintWindow, "BLUE", (180, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.paintWindow, "GREEN", (300, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2, cv2.LINE_AA)
+
+    def clear_canvas(self):
+        self.blue = [deque(maxlen=1024)]
+        self.green = [deque(maxlen=1024)]
+        self.red = [deque(maxlen=1024)]
+        self.blue_index = 0
+        self.green_index = 0
+        self.red_index = 0
+        self.paintWindow[67:, :, :] = 255
+
+    def update_color_index(self, index):
+        self.colorIndex = index
+
+    def add_point(self, center):
+        if self.colorIndex == 0:
+            self.blue[self.blue_index].appendleft(center)
+        elif self.colorIndex == 1:
+            self.green[self.green_index].appendleft(center)
+        elif self.colorIndex == 2:
+            self.red[self.red_index].appendleft(center)
+
+    def get_points(self):
+        return [self.blue, self.green, self.red]
+    
+    def add_next_deques(self):
+        self.blue.append(deque(maxlen=512))
+        self.blue_index += 1
+        self.green.append(deque(maxlen=512))
+        self.green_index += 1
+        self.red.append(deque(maxlen=512))
+        self.red_index += 1
+
+    @staticmethod
+    def update_camera_ui(frame):
+        frame = cv2.rectangle(frame, (40, 1), (140, 65), (0, 0, 0), 2)
+        frame = cv2.rectangle(frame, (160, 1), (260, 65), (255, 0, 0), 2)
+        frame = cv2.rectangle(frame, (280, 1), (380, 65), (0, 255, 0), 2)
+        frame = cv2.rectangle(frame, (400, 1), (500, 65), (0, 0, 255), 2)
+        cv2.putText(frame, "CLEAR", (60, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "BLUE", (180, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "GREEN", (300, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2, cv2.LINE_AA)
+        return frame
+
+
+    
+
